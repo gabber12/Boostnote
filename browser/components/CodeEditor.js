e50949400bd6106184e93aa07283e7c18afd10b1
Shubham Sharma(gabber12) <shubham.sha12@gmail.com>
2017-05-14 21:28:47 +0530

    ++ md shortcut bold and italicize

--- browser/components/CodeEditor.js
+++ browser/components/CodeEditor.js
@@ -56,7 +56,7 @@ export default class CodeEditor extends React.Component {
       keyMap: this.props.keyMap,
       inputStyle: 'textarea',
       dragDrop: false,
-      extraKeys: {
+      extraKeys: Object.assign({
         Tab: function (cm) {
           const cursor = cm.getCursor()
           const line = cm.getLine(cursor.line)
@@ -80,11 +80,8 @@ export default class CodeEditor extends React.Component {
             }
           }
         },
-        'Cmd-T': function (cm) {
-          // Do nothing
-        },
         Enter: 'newlineAndIndentContinueMarkdownList'
-      }
+      }, this.props.extraKeys)
     })
 
     this.setMode(this.props.mode)
--- browser/components/MarkdownEditor.js
+++ browser/components/MarkdownEditor.js
@@ -201,7 +201,59 @@ class MarkdownEditor extends React.Component {
   handleLockEditor () {
     this.setState({ isLocked: !this.state.isLocked })
   }
+  insertAround(doc, start, end) {
 
+     var cursor = doc.getCursor();
+
+     if (doc.somethingSelected()) {
+         var selection = doc.getSelection();
+         doc.replaceSelection(start + selection + end);
+     } else {
+         // If no selection then insert start and end args and set cursor position between the two.
+         doc.replaceRange(start + end, { line: cursor.line, ch: cursor.ch });
+         doc.setCursor({ line: cursor.line, ch: cursor.ch + start.length })
+     }
+  }
+  insertBefore(doc, insertion, cursorOffset) {
+
+      var cursor = doc.getCursor();
+
+      if (doc.somethingSelected()) {
+          var selections = doc.listSelections();
+          selections.forEach(function(selection) {
+              var pos = [selection.head.line, selection.anchor.line].sort();
+
+              for (var i = pos[0]; i <= pos[1]; i++) {
+                  doc.replaceRange(insertion, { line: i, ch: 0 });
+              }
+
+              doc.setCursor({ line: pos[0], ch: cursorOffset || 0 });
+          });
+      } else {
+          doc.replaceRange(insertion, { line: cursor.line, ch: 0 });
+          doc.setCursor({ line: cursor.line, ch: cursorOffset || 0 })
+      }
+  }
+  getExtraKeys () {
+    return {
+      'Ctrl-B':  (cm) => {
+        this.insertAround(cm, '**', '**')
+      },
+      'Ctrl-I': (cm) => {
+        this.insertAround(cm, '*', '*')
+      },
+      "Ctrl-'": (cm) => {
+        this.insertBefore(cm, '>', '2')
+      },
+      "Ctrl-Alt-L" : (cm) => {
+        this.insertBefore(cm, '1. ', 3);
+      },
+      "Ctrl-L": (cm) => {
+        this.insertBefore(cm, '* ', 2);
+      },
+      ""
+    }
+  }
   render () {
     let { className, value, config } = this.props
 
@@ -235,6 +287,7 @@ class MarkdownEditor extends React.Component {
           indentSize={editorIndentSize}
           onChange={(e) => this.handleChange(e)}
           onBlur={(e) => this.handleBlur(e)}
+          extraKeys={this.getExtraKeys()}
         />
         <MarkdownPreview styleName={this.state.status === 'PREVIEW'
             ? 'preview'
